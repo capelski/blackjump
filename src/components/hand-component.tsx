7334e536f4c9fa47e4fd0a69408120357b40c017
Carles Capellas <capellas.carles@gmail.com>
2020-10-08 13:28:01 +0200

    fix: display blackjack instead of 11/21

--- src/components/hand-component.tsx
+++ src/components/hand-component.tsx
@@ -9,31 +9,37 @@ interface HandComponentProps {
     isCurrentHand: boolean;
 }
 
-export const HandComponent: React.FC<HandComponentProps> = (props) => (
-    <View
-        style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            maxWidth: '100%',
-            paddingTop: 8,
-            paddingHorizontal: 8,
-            backgroundColor: props.isCurrentHand ? 'rgba(255, 255, 255, 0.2)' : undefined
-        }}
-    >
-        {props.hand.cards.map((card, index) => (
-            <CardComponent key={index} card={card} />
-        ))}
-        <View>
-            <Text style={{ fontSize: 25, color: 'white' }}>
-                {' '}
-                {getHandValidValues(props.hand).join('/')}
-            </Text>
-            {props.hand.outcome && (
-                <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>
-                    {' '}
-                    {props.hand.outcome}
-                </Text>
-            )}
+export const HandComponent: React.FC<HandComponentProps> = (props) => {
+    const handValues = getHandValidValues(props.hand).join('/');
+    const displayValues =
+        handValues.indexOf('21') > -1
+            ? props.hand.cards.length === 2
+                ? 'Blackjack'
+                : '21'
+            : handValues;
+    return (
+        <View
+            style={{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                maxWidth: '100%',
+                paddingTop: 8,
+                paddingHorizontal: 8,
+                backgroundColor: props.isCurrentHand ? 'rgba(255, 255, 255, 0.2)' : undefined
+            }}
+        >
+            {props.hand.cards.map((card, index) => (
+                <CardComponent key={index} card={card} />
+            ))}
+            <View>
+                <Text style={{ fontSize: 25, color: 'white' }}> {displayValues}</Text>
+                {props.hand.outcome && (
+                    <Text style={{ fontSize: 25, color: 'white', fontWeight: 'bold' }}>
+                        {' '}
+                        {props.hand.outcome}
+                    </Text>
+                )}
+            </View>
         </View>
-    </View>
-);
+    );
+};
