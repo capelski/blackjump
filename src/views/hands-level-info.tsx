bdefe2f36cb9b2773ef4dee8c1b81cc69285c63d
Carles Capellas <capellas.carles@gmail.com>
2021-01-05 18:48:26 +0100

    feat: display all hands level on hands level info

--- src/views/hands-level-info.tsx
+++ src/views/hands-level-info.tsx
@@ -1,72 +1,140 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
 import { NavigationScreenProp } from 'react-navigation';
+import { CasinoRuleSwitch } from '../components/casino-rule-switch';
 import { HandComponent } from '../components/hand-component';
+import { HandDecisionsTable } from '../components/hand-decisions-table';
 import { WithNavBar, WithNavBarPropsFromScreenProps } from '../components/with-nav-bar';
-import { CardSuit, SimpleCardSymbol } from '../types';
+import { handToRelevantHand } from '../logic/basic-strategy';
+import { decisionsDictionary } from '../logic/decisions-dictionary';
+import {
+    CardSuit,
+    CasinoRulesKeys,
+    Dictionary,
+    GameConfig,
+    Hand,
+    SimpleCardSymbol
+} from '../types';
+
+interface HandsLevelInfoProps extends WithNavBarPropsFromScreenProps {
+    gameConfig: GameConfig;
+}
+
+const levelsColor: Dictionary<string, number> = {
+    1: 'white',
+    2: 'yellow',
+    3: 'orange',
+    4: 'lightcoral'
+};
 
 export const HandsLevelInfo: React.FC<{
     navigation: NavigationScreenProp<{ routeName: string }>;
-    screenProps: WithNavBarPropsFromScreenProps;
-}> = ({ navigation, screenProps }) => (
-    <WithNavBar
-        navigation={navigation}
-        player={screenProps.player}
-        totalAttemptedDecisions={screenProps.totalAttemptedDecisions}
-        totalRightDecisions={screenProps.totalRightDecisions}
-    >
-        <ScrollView
-            style={{
-                margin: 16,
-                overflow: 'scroll'
-            }}
-            contentContainerStyle={{
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
+    screenProps: HandsLevelInfoProps;
+}> = ({ navigation, screenProps }) => {
+    const [casinoRules, setCasinoRules] = useState(screenProps.gameConfig.casinoRules);
+
+    const hardEight: Hand = {
+        bet: 1,
+        cards: [
+            { suit: CardSuit.clubs, symbol: SimpleCardSymbol.Five },
+            { suit: CardSuit.hearts, symbol: SimpleCardSymbol.Three }
+        ],
+        values: [8]
+    };
+    const splitNine: Hand = {
+        bet: 1,
+        cards: [
+            { suit: CardSuit.spades, symbol: SimpleCardSymbol.Nine },
+            { suit: CardSuit.diamonds, symbol: SimpleCardSymbol.Nine }
+        ],
+        values: [18]
+    };
+
+    return (
+        <WithNavBar
+            navigation={navigation}
+            player={screenProps.player}
+            totalAttemptedDecisions={screenProps.totalAttemptedDecisions}
+            totalRightDecisions={screenProps.totalRightDecisions}
         >
-            <Text style={{ color: 'white', fontSize: 20, marginBottom: 16 }}>
-                A hand level tells how many different optimal actions are for that hand depending on
-                the dealer's up card. For example:
-            </Text>
-
-            <HandComponent
-                hand={{
-                    bet: 1,
-                    cards: [
-                        { suit: CardSuit.clubs, symbol: SimpleCardSymbol.Five },
-                        { suit: CardSuit.hearts, symbol: SimpleCardSymbol.Three }
-                    ],
-                    values: [8]
+            <ScrollView
+                style={{
+                    margin: 16,
+                    overflow: 'scroll'
                 }}
-                isCurrentHand={false}
-            ></HandComponent>
-            <Text style={{ color: 'white', fontSize: 20, marginBottom: 16 }}>
-                The optimal decision for a Hard 8 is to always Hit, no matter the dealer's up card.
-                Thus the hand level is 1.
-            </Text>
-
-            <HandComponent
-                hand={{
-                    bet: 1,
-                    cards: [
-                        { suit: CardSuit.spades, symbol: SimpleCardSymbol.Nine },
-                        { suit: CardSuit.diamonds, symbol: SimpleCardSymbol.Nine }
-                    ],
-                    values: [18]
+                contentContainerStyle={{
+                    alignItems: 'center',
+                    justifyContent: 'center'
                 }}
-                isCurrentHand={false}
-            ></HandComponent>
-            <Text style={{ color: 'white', fontSize: 20, marginBottom: 16 }}>
-                The optimal decisions for a 9,9 are to Split from 2 to 6, Stand on 7, Split from 8
-                to 9 and Stand from 10 to A. A total of 4 groups of actions, thus the hand level is
-                4.
-            </Text>
+            >
+                <Text style={{ color: 'white', fontSize: 20, marginBottom: 16 }}>
+                    A hand level tells how many different actions there are for that hand depending
+                    on the dealer's up card.
+                </Text>
 
-            <Text style={{ color: 'white', fontSize: 20 }}>
-                Keep in mind that the game settings affect the level of some hands (e.g. Hard 16 is
-                level 2, but becomes level 3 when Surrender action is enabled).
-            </Text>
-        </ScrollView>
-    </WithNavBar>
-);
+                <Text style={{ color: 'white', fontSize: 20, marginBottom: 16 }}>
+                    For example, a Hard 8 is level 1 because the optimal action is always Hit, no
+                    matter the dealer's up card.
+                </Text>
+
+                <HandComponent hand={hardEight} isCurrentHand={false} />
+                <HandDecisionsTable
+                    casinoRules={screenProps.gameConfig.casinoRules}
+                    relevantHand={handToRelevantHand(hardEight)}
+                />
+
+                <Text style={{ color: 'white', fontSize: 20, marginBottom: 16, marginTop: 32 }}>
+                    A 9,9 however is level 4 because it has 4 groups of optimal actions depending on
+                    the dealer's up card: Split (dealer's 2 to 6), Stand (dealer's 7), Split
+                    (dealer's 8 and 9) and Stand (dealer's 10 and A).
+                </Text>
+
+                <HandComponent hand={splitNine} isCurrentHand={false} />
+                <HandDecisionsTable
+                    casinoRules={screenProps.gameConfig.casinoRules}
+                    relevantHand={handToRelevantHand(splitNine)}
+                />
+
+                <Text style={{ color: 'white', fontSize: 20, marginBottom: 16, marginTop: 32 }}>
+                    Finally, keep in mind that the casino rules modify the level of some hands. For
+                    example, Hard 16 is level 2 when Surrender is disabled but it becomes level 3
+                    when Surrender is enabled. Here is a list of each hand level:
+                </Text>
+
+                <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                    {Object.values(decisionsDictionary).map((relevantHand) => {
+                        const level = relevantHand.level(casinoRules);
+                        return (
+                            <Text
+                                key={relevantHand.name}
+                                style={{
+                                    color: levelsColor[level],
+                                    fontSize: 20,
+                                    marginVertical: 4,
+                                    textAlign: 'center',
+                                    width: '33%'
+                                }}
+                            >
+                                {relevantHand.name}: {level}
+                            </Text>
+                        );
+                    })}
+                </View>
+
+                <View style={{ marginBottom: 16 }}>
+                    {Object.values(CasinoRulesKeys).map((casinoRule) => (
+                        <CasinoRuleSwitch
+                            casinoRule={casinoRule}
+                            key={casinoRule}
+                            onValueChange={(newValue) => {
+                                const nextCasinoRules = { ...casinoRules, [casinoRule]: newValue };
+                                setCasinoRules(nextCasinoRules);
+                            }}
+                            value={casinoRules[casinoRule]}
+                        />
+                    ))}
+                </View>
+            </ScrollView>
+        </WithNavBar>
+    );
+};
